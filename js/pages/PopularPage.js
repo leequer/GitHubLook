07d81e5c57b634403e2039bb0956c8fbd6ea07f6
leequer <leequer@qq.com>
2017-05-31 17:23:06 +0800

    填写tab中的listview 固定数据

--- js/pages/PopularPage.js
+++ js/pages/PopularPage.js
@@ -7,7 +7,8 @@ import {
     StyleSheet,
     Text,
     View,
-    Image
+    Image,
+    ListView
 } from 'react-native';
 import NavigationBar from '../../js/Component/NavigationBar.js';
 import ScrollableTabView from 'react-native-scrollable-tab-view';
@@ -31,7 +32,7 @@ export default class PopularPage extends Component {
                 >
                     {
                         this.status.languages.map((item,i)=>{
-                            return <Text tabLabel={item} key={i}/>
+                            return <PopularTab tabLabel={item} key={i}/>
                         })
                     }
 
@@ -41,7 +42,23 @@ export default class PopularPage extends Component {
         )
     }
 }
-
+class PopularTab extends Component{
+    constructor(props){
+        super(props);
+        var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+        this.state = {
+            dataSource: ds.cloneWithRows(['row 1', 'row 2']),
+        };
+    }
+    render() {
+        return (
+            <ListView
+                dataSource={this.state.dataSource}
+                renderRow={(rowData) => <Text>{rowData}</Text>}
+            />
+        );
+    }
+}
 const styles = StyleSheet.create({
     container: {
         flex: 1,
