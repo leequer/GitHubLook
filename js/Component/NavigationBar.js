da56f830d00ce00148ea4d69d154b473071eecc9
leequer <leequer@qq.com>
2017-05-31 15:53:32 +0800

    <StatusBar hidden={Platform.OS==='ios'?false:true} barStyle={Platform.OS==='ios'?'light-content':null}/> 根据平台不同  决定状态栏显示方式  ios 不隐藏状态栏 并且是light-content样式，android隐藏状态栏没有样式

--- js/Component/NavigationBar.js
+++ js/Component/NavigationBar.js
@@ -8,63 +8,74 @@ import {
     Text,
     View,
     Image,
-    StatusBar
+    StatusBar,
+    TouchableOpacity,
+    Platform
 } from 'react-native';
 
 export default class NavigationBar extends Component {
-    render(){
-        return(
+    render() {
+        return (
             <View style={styles.container}>
-                <StatusBar hidden={true} barStyle={'light-content'}/>
-                <View style={styles.navigationBar} >
+                <StatusBar hidden={Platform.OS==='ios'?false:true} barStyle={Platform.OS==='ios'?'light-content':null}/>
+                <View style={styles.navigationBar}>
                     <View style={styles.icon}/>
-                    <View  style={styles.navigationBarText}>
-                    <Text style={styles.BarText}>热门</Text>
+                    <View style={styles.navigationBarText}>
+                        <Text style={styles.BarText}>热门</Text>
                     </View>
                     <View style={styles.navigationBarRihgt}>
-                        <Image style={styles.icon} source={require('../../res/images/ic_search_white_48pt.png')}/>
-                        <Image style={styles.icon} source={require('../../res/images/ic_more_vert_white_48pt.png')}/>
+                        <TouchableOpacity activeOpacity={0.7}>
+                            <Image style={styles.icon}
+                                   source={require('../../res/images/ic_search_white_48pt.png')}/>
+                        </TouchableOpacity>
+                        <TouchableOpacity activeOpacity={0.7}>
+                            <Image style={styles.icon}
+                                   source={require('../../res/images/ic_more_vert_white_48pt.png')}/>
+                        </TouchableOpacity>
                     </View>
                 </View>
             </View>
-    )
+        )
     }
 }
 
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        backgroundColor:'#CCC',
-        padding:5
+        backgroundColor: '#63b8ff',
+        padding: 5
     },
-    navigationBarText:{
+   navigationBarText: {
         //中间文字编辑剧中-begin
-        flexDirection:'column',
-        alignItems:'center', //交叉轴
-        position:'absolute',
-        left:50,
-        right:50,
-        bottom:0,
+        flexDirection: 'column',
+        alignItems: 'center', //交叉轴
+        position: 'absolute',
+        left: 50,
+        right: 50,
+        bottom: 1,
         //中间文字居中-end
         //backgroundColor:'#f00'
 
     },
-    BarText:{
-        fontSize:16,
+    BarText: {
+        fontSize: 16,
     },
-    navigationBar:{
-        flexDirection:'row',
-        justifyContent:'space-between', //等分
-        alignItems:'center' //交叉轴
+    navigationBar: {
+        flexDirection: 'row',
+        justifyContent: 'space-between', //等分
+        alignItems: 'center', //交叉轴
+        marginTop:30
+
     },
-    navigationBarRihgt:{
-        flexDirection:'row',
-        justifyContent:'flex-end',
+    navigationBarRihgt: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        marginRight:8,
+
     },
-    icon:{
-        width:24,
-        height:24,
+    icon: {
+        width: 28,
+        height: 28,
     }
 
 });
