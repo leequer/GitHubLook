e9c3545d0e8a06560bdf7a1e17354508845a5f4b
leequer <leequer@qq.com>
2017-06-01 14:21:58 +0800

    item样式

--- js/Component/ProjectRow.js
+++ js/Component/ProjectRow.js
@@ -20,23 +20,61 @@ export default class ProjectRow extends Component {
      * 这个item 就是 这个ProjectRow中的自定义属性 item 就在这里写 写成这样
      * @type {{item: {}}}
      */
-    static defaultProps ={
-        item :{}
+    static defaultProps = {
+        item: {}
     }
+
     render() {
         var item = this.props.item;
-            return (
-                <View style ={styles.container}>
-                    <Text>-->{item.full_name}</Text>
+        return (
+            <View style={styles.container}>
+                <Text style={styles.nameSytle}>{item.name}</Text>
+                <Text style={{color:'#757575'}}>{item.description}</Text>
+                <View style={styles.bottomView}>
+                    <View style={styles.avatarView}>
+                        <Text>作者：</Text>
+                        <Image source={{url: item.owner.avatar_url}} style={[{height: 20}, {width: 20}]}/>
+                    </View>
+                    <View style={styles.startView}>
+                        <Text>Starts:</Text>
+                        <Text>{item.stargazers_count}</Text>
+                    </View>
+                    <View style={styles.starView}>
+                        <Image source={require('../../res/images/ic_unstar_navbar.png')} style={[{height:25},{width:25}]}/>
+                    </View>
                 </View>
-            )
+            </View>
+        )
     }
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        marginLeft:10,
+        padding:5
+    },
+    nameSytle: {
+        marginBottom:5,
+        fontSize: 18
+    },
+    avatarView:{
+        flexDirection: 'row',
+        alignItems:'center',
 
     },
+    startView:{
+        flexDirection: 'row',
+        marginLeft:10
+    },
+    starView:{
+      marginRight:10
+    },
+    bottomView: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginTop: 5,
+        justifyContent: 'space-between', //等分
+    }
 
 });
